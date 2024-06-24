f67e103dc07ed28a5c5b45a6b56ab6a2fc016595
RudakovskyS <rudakovsky_sd@mail.ru>
2024-06-24 23:32:39 +0300

    add project for authentication service

---
+++ auth-reg-service/src/test/java/by/srudakovsky/auth_reg_service/AuthRegServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package by.srudakovsky.auth_reg_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthRegServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
